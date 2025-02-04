47ad26aa28c5e030b195f5a546599409fcddb4b0
ktran246 <ktran246@uwo.ca>
2025-02-03 21:29:16 -0500

    added background color on hover

---
+++ script.js
@@ -0,0 +1,10 @@
+// Onload functions 
+window.onload = function() {
+    prepareListeners();
+}
+
+// Sets up listeners
+function prepareListeners() {
+    // let element = document.getElementById("elementId");
+    // element.addEventListener("click", someFunction);
+}
