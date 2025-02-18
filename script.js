160b0abfac6cc4f999c1bf5d9e4bff459c42c183
Dylan <dylantrojek@outlook.com>
2025-02-18 15:27:58 -0500

    updated navbar

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function prepareListeners() {
     // let element = document.getElementById("elementId");
     // element.addEventListener("click", someFunction);
 }
+
+// d navbar 
+// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
